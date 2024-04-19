0e3579ed1f6cc283849f854de93e65ec6034f11c
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-19 06:48:24 +0300

    ICONS MODULE PATH WORKED  ON

--- app/src/main/assets/Projects/Modules/IconsModule.js
+++ app/src/main/assets/Projects/Modules/IconsModule.js
@@ -1,4 +1,4 @@
-const ICONMODULE='../Library/Icons/';
+const ICONMODULE='https://e-corpcompanygroupmovieslander.github.io/Movie_Lander_Build/app/src/main/assets/Library/Icons/';
 
 
 export{ICONMODULE}
