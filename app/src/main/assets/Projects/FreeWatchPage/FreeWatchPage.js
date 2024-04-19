52304b465564e68bcb637e80e64c03ce65459e86
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-19 22:12:32 +0300

    VERSION 3 RELEASE

--- app/src/main/assets/Projects/FreeWatchPage/FreeWatchPage.js
+++ app/src/main/assets/Projects/FreeWatchPage/FreeWatchPage.js
@@ -1,3 +1,4 @@
+import { FREEMOVIESAPI } from "../../Modules/Module.js";
 import { HOMEPAGE } from "../HomePage/HomePage.js"
 
 export const FREEWATCHPAGE=()=>{
@@ -7,7 +8,27 @@ export const FREEWATCHPAGE=()=>{
             <h1 class='Profile'>Free Watch</h1>
         
         `,
-        ``
+        ``,'FreeWatchDiv'
     );
+
+    GETPACKAGE(FREEMOVIESAPI,'cors',(data)=>{
+
+        DECLARATION('#FreeWatchDiv',(ELEMENT)=>{
+
+            DISPLAY(ELEMENT,data)
+
+            REDUX(data,(element)=>{
+
+                CREATEELEMENT('div','FreeDivHolder',(HOLDER)=>{
+
+
+
+                })
+    
+            })
+
+        })
+
+    })
 
 }
