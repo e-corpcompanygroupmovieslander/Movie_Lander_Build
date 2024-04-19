b263816b17b9313bee6d2bd87e2379d68c3c8dc1
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-19 08:35:13 +0300

    LOGIN PAGE

--- app/src/main/assets/Connection/Connection.js
+++ app/src/main/assets/Connection/Connection.js
@@ -1,55 +1,7 @@
 import { HOMEPAGE } from "../Projects/HomePage/HomePage.js";
-import { LOGINPAGE } from "../Projects/LoginPage/LoginPage.js";
+import { LOGINPAGE } from "../Projects/Pages.js";
+
 const CONNECTION = () => {
-
-    CHECK('',(result)=>{
-
-        GETPACKAGE('https://script.googleusercontent.com/macros/echo?user_content_key=lAYYsAjQVbByXET_kfABc7Aa7XCV4MDRw1G6EJEuqswiRKP0nyFFlmSZYG60mGKTbiljX4IK6hSM_g9s6ADFUwAyZC-fnFkRm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnF2Q8H1LAVtp-6OIwol2w20OiUzRP9L1GSLipr58ni3wZ3MWwAFSxGtWq7rEpkhOyEAbUlUeLRUiQmJN2-ivIdekd4xJLH4V-w&lib=MzWrqK870Yn5uC3rxsYUWkMSoMAbC-O1l','cors',(data)=>{
-        
-            FINDER(data,'User',localStorage.getItem('User'),(user)=>{
-
-                CONDITION(user.User === localStorage.getItem('User'),
-
-                ()=>CHECK(user,(result)=>{
-
-                    REMOVESTORE('local','User'),REMOVESTORE('local','UserData');
-
-                    REMOVESTORE('local','Premium'),REMOVESTORE('local','PremiumUser');
-
-                    LOGINPAGE();
-    
-                }),
-
-                ()=>CHECK(user,(result)=>{
-
-                    console.log('Account Not Deleted');
-                    
-                })
-                
-                )
-
-            })
-
-        })
-        
-        GETPACKAGE('https://script.googleusercontent.com/macros/echo?user_content_key=kYLhamRqUj16-aL1rdJ_VtjUbS7NaKp1k04OM4xZ29mCQO9sX8YRvyz_MNUVPM65knFD3UoNIC1vWmx_XngMw7UoyevKxdE6m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnB6qUJyy0-nbgIYqHld3JutVMF0GqNpXCKea3hKLtzI0ajmYczC2uRoJrh6ibkKg85PhHPJVPaEYuGB3l_Sk1xmqIpQGL8q6bw&lib=Mp7RLsbiuB3aWCASYYtzoScSoMAbC-O1l', 'cors', (data) => {
-            REVERSE(data,(result)=>{
-                FINDER(result, 'User', localStorage.getItem('User'), (user) => {
-                    if (new Date() <= new Date(user.ExpiryDate)) {
-                        CHECK(user, (result) => {
-                            STORE('local', 'PremiumUser', JSON.stringify(result));
-                            STORE('local', 'Premium', 'ON');
-                        });
-                    } else {
-                        REMOVESTORE('local', 'PremiumUser');
-                        REMOVESTORE('local', 'Premium');
-                        console.log('no premium')
-                    }
-                });
-            });    
-        });
-         
-    })
 
     CONDITION(
         localStorage.getItem("User"),
--- app/src/main/assets/Modules/Module.js
+++ app/src/main/assets/Modules/Module.js
@@ -4,5 +4,7 @@ const ICONMODULE='https://e-corpcompanygroupmovieslander.github.io/Movie_Lander_
 const LOGINMODULE=(BUTTON,INPUT,INPUT2,LINK,INPUTREF,INPUTREF2,callback)=>{const Button=document.querySelector(BUTTON);const EMAIL=document.querySelector(INPUT);const PASSWORD=document.querySelector(INPUT2);CONDITION(EMAIL.value,()=>CONDITION(PASSWORD.value,()=>CHECK(Button,(result)=>{LOADER(Button); GETPACKAGE(LINK,'cors',(data)=>{FINDER(data,INPUTREF,EMAIL.value,(user)=>{CONDITION(user[INPUTREF]===EMAIL.value,()=>CONDITION(user[INPUTREF2]===PASSWORD.value,()=>callback(user),()=>CHECK(user,(result)=>{MESSAGE('Wrong User Password'),ORIGIN(Button,'Login')})),()=>CHECK(user,(result)=>{MESSAGE('Wrong User Email'),ORIGIN(Button,'Login')}))})})}),()=>MESSAGE('Enter User Password')),()=>MESSAGE('Enter User Email'))}
 //DELETE ACCOUNT MODULE
 const DELETEDACCOUNTMODULE=(LINK,INPUT,INPUTREF,callback)=>{GETPACKAGE(LINK,'cors',(data)=>{FINDER(data,INPUT,INPUTREF,(user)=>{CONDITION(user[INPUTREF]===INPUT,()=>callback(!0),()=>callback(!1))})})}
+//POST DEVICE DATA
+const POSTDEVICEMODULE=(USER,LINK,callback)=>{function getBrowserVersion(){return navigator.appVersion}const DEVICEDATA={"User":USER,"Device":getBrowserVersion(),"Date":new Date()};POSTPACKAGE(LINK,'no-cors',DEVICEDATA,(data)=>{callback(data)})}
 
-export{ICONMODULE,LOGINMODULE,DELETEDACCOUNTMODULE}
+export{ICONMODULE,LOGINMODULE,DELETEDACCOUNTMODULE,POSTDEVICEMODULE}
