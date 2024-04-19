12bdb9fbbbee1f9bc6fc227497c29f1654611f60
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-20 01:10:29 +0300

    VERSION 4

--- app/src/main/assets/Projects/HomePage/HomeMovies/LatestAnimation/LatestAnimation.js
+++ app/src/main/assets/Projects/HomePage/HomeMovies/LatestAnimation/LatestAnimation.js
@@ -10,8 +10,12 @@ export const LATESTANIMATION=()=>{
 
                 DISPLAY(ELEMENT,`
 
-                    <img class='LatestImage' src='${MOVIESPATH+data.MovieImage}'/>
-                
+                    <img class='LatestImage' src='${MOVIESPATH + data.MovieImage}'/>
+                            
+                    <button class='About'>Watch</button>
+
+                    <h1 id='LatestTitle' class='UserName'>${data.MovieName}</h1>
+                        
                 `)
 
             })
