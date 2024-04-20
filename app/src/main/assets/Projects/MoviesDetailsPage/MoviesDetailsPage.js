7c100d482229a5c8c0ec016617ca812d6d64229d
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-20 11:23:10 +0300

    MOVIE DETAILS PAGE

--- app/src/main/assets/Projects/MoviesDetailsPage/MoviesDetailsPage.js
+++ app/src/main/assets/Projects/MoviesDetailsPage/MoviesDetailsPage.js
@@ -17,6 +17,55 @@ export const MOVIESDETAILSPAGE=()=>{
             <h1 class='Profile'>${data.MovieName}</h1>
 
         </header>
+
+        <div class='MovieDetailsDataDiv'>
+
+            <iframe class='TrailorHolder' src='${YOUTUBELINK+data.MoveTrailer}'/></iframe>
+            
+            <button class='brown'>Watch</button>
+
+            <div class='DataDisplayDiv'>
+
+                <h1 id='Movietitle' class='Message'>${data.MovieName}</h1>
+
+                <button class='MyDataButton'>
+
+                    <h1 class='mytitle'>Rating</h1>
+
+                    <img id='RatingIcon' class='myIcon' src='${ICONMODULE}trending-topic.png'/>
+                
+                    <h1 class='MyData'>${data.Rated}/10</h1>
+
+                    <h1 class='Catergories'>${data.Catergory}</h1>
+
+                    <footer class='DetailsFooter'>
+
+                        <img id='Catergories' src='${ICONMODULE}lock.png'/>
+
+                        <img id='ParentalControl' src='${ICONMODULE}PG.png'/>
+
+                        <img id='SaveMovie' src='${ICONMODULE}save.png'/>
+                                        
+                    </footer>
+
+                </button>
+
+            </div>
+            
+            <footer class='Footer'>
+
+                <button class='Bottomdata'>Insight</button>
+
+                <button id='Moviedata' class='Bottomdata'>Details</button>
+
+                <button id='MovieComments' class='Bottomdata'>Comments</button>
+            
+            </footer>
+            
+        
+        </div>
+        
+        </div>
         
         
         `);
@@ -25,7 +74,7 @@ export const MOVIESDETAILSPAGE=()=>{
 
             CONDITION(sessionStorage.getItem('Path') === 'CATERGORYPAGE' ,
         
-            ()=>CATERGORYPAGE(),
+            ()=>MOVIESSELECTIONPAGE(),
     
             ()=>HOMEPAGE()
         
