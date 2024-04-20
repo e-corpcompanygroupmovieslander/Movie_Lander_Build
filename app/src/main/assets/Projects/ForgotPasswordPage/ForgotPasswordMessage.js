5c32b2384b17f96495deddae8f27f9843a53ef19
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-20 21:24:30 +0300

    VERSION 4 RELEASE

--- app/src/main/assets/Projects/ForgotPasswordPage/ForgotPasswordMessage.js
+++ app/src/main/assets/Projects/ForgotPasswordPage/ForgotPasswordMessage.js
@@ -6,7 +6,14 @@ export const FORGOTPASSWORDMESSAGEPAGE=()=>{
 
         <img class='AppLogo' src='../Library/Images/app_icon.png'/>
 
-        <h1 class='LoaderMessage'>Password Sent To Email</h1>
+        <h1>Password Recovered</h1>
+
+        <p>
+            User Password is Sent  <br><br> 
+            ${sessionStorage.getItem('User')}
+            <br><br>
+            Check Email to get Your Password.
+        </p>
         
         <button class='teal' >Back</button>
 
--- app/src/main/assets/Projects/ForgotPasswordPage/ForgotPasswordPage.js
+++ app/src/main/assets/Projects/ForgotPasswordPage/ForgotPasswordPage.js
@@ -1,3 +1,4 @@
+import { EMAILSENDERAPI, LOGINAPI } from "../../Modules/Module.js"
 import { LOGINPAGE } from "../LoginPage/LoginPage.js"
 import { FORGOTPASSWORDMESSAGEPAGE } from "./ForgotPasswordMessage.js"
 
@@ -7,7 +8,7 @@ export const FORGOTPASSWORDPAGE=()=>{
 
         <img class='AppLogo' src='../Library/Images/app_icon.png'/>
 
-        <input type='email' placeholder='Enter Email'/>
+        <input class='Email' type='email' placeholder='Enter Email'/>
         
         <button class='forestgreen' >Recover</button>
 
@@ -17,6 +18,40 @@ export const FORGOTPASSWORDPAGE=()=>{
 
     CLICKED('.brown',()=>{LOGINPAGE()})
 
-    CLICKED('.forestgreen',()=>{FORGOTPASSWORDMESSAGEPAGE()})
+    CLICKED('.forestgreen',()=>{
+
+        const Email=document.querySelector('.Email');
+        const BUTTON=document.querySelector('.forestgreen');
+
+        CONDITION(Email.value,
+            ()=>CHECK(Email.value,(result)=>{
+                LOADER(BUTTON);
+                GETPACKAGE(LOGINAPI,'cors',(data)=>{
+                    FINDER(data,'Email',Email.value,(user)=>{
+                        CONDITION(user.Email === Email.value,
+                            ()=>CHECK(user,(result)=>{
+                                STORE('','User',user.Email)
+                                var EMAILDATA = {
+                                    recipientEmail: user.Email,
+                                    subject: "Movie Lander Password Recovery",
+                                    body: `Dear ${user.UserName},\n\nThank you for using Movie Lander. To complete your Login Into Your Account, Your Account Password Is:\n\nPassword: ${user.Password}\n\nOpen Movie Lander And Login Using Your Password Above .\n Don't Share Your Password. If you did not request this code, please ignore this email.\n\nBest regards, Movie Lander Team\n\n https://www.e-corpcompanygroup.com`
+                                };
+                                POSTPACKAGE(EMAILSENDERAPI,'no-cors',EMAILDATA,(data)=>{
+                                    FORGOTPASSWORDMESSAGEPAGE();
+                                })
+                            }),
+                            ()=>CHECK(user,(result)=>{
+                                MESSAGE('Email Not Found');
+                                ORIGIN(BUTTON,'Recover')
+                            })
+                        )
+                    })
+                })
+                }
+            ),
+            ()=>MESSAGE('Enter User Email')
+        )
+
+    })
     
 }
