e9e3482b5f8e1baac9b268fc05dc149e64444505
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-20 22:14:39 +0300

    VERSION 4 RELEASE

--- app/src/main/assets/Projects/CreateAccountPage/CreateAccountPage.js
+++ app/src/main/assets/Projects/CreateAccountPage/CreateAccountPage.js
@@ -1,3 +1,4 @@
+import { ICONMODULE } from "../../Modules/Module.js";
 import { EMAILVERIFICATIONPAGE } from "../EmailVerificationPage/EmailVerificationPage.js";
 import { LOGINPAGE } from "../LoginPage/LoginPage.js";
 
@@ -7,26 +8,92 @@ export const CREATEACCOUNTPAGE=()=>{
 
         <img class='AppLogo' src='../Library/Images/app_icon.png'/>
 
-        <input type='text' placeholder='Enter User Name'/>
+        <input class='Username' type='text' placeholder='Enter User Name'/>
 
-        <input type='email' placeholder='Enter User Email'/>
+        <input class='UserEmail' type='email' placeholder='Enter User Email'/>
 
-        <input type='password' placeholder='Enter User Password'/>
+        <input class='UserPassword' type='password' placeholder='Enter User Password'/>
 
-        <input type='text' placeholder='Enter Birth day'/>
+        <input class='UserDate' type='text' placeholder='Enter Birth day'/>
 
-        <input type='text' placeholder='Enter User Location'/>
+        <input class='Location' type='text' placeholder='Enter User Location' ReadOnly/>
 
-        <input type='tel' maxlength='10'   placeholder='Enter User Telephone'/>
+        <h1 class='DisplayCode' >+</h1>
+
+        <input class='Telephone' type='tel' maxlength='10'   placeholder='Enter User Telephone'/>
         
         <button class='forestgreen' >Create Account</button>
 
         <button class='blue' >Login</button>
 
+        <div class='CountryDiv'>
+            
+            <button class='brown'>Close</button>
+
+            <div class='CoutryDivs'></div>
+        
+        </div>
+
     `);
 
     CLICKED('.blue',()=>{LOGINPAGE()});
 
-    CLICKED('.forestgreen',()=>{EMAILVERIFICATIONPAGE()})
+    CLICKED('.UserDate',()=>{
+        DECLARATION('.UserDate',(ELEMENT)=>{
+            ELEMENT.type='date';
+        })
+    })
+
+    CLICKED('.Location',()=>{
+
+        const CoutryDivs=document.querySelector('.CoutryDivs');
+
+        const CountryDiv=document.querySelector('.CountryDiv');
+
+        const Location = document.querySelector('.Location');
+
+        const Code =document.querySelector('.DisplayCode');
+
+        STYLED(CountryDiv,'display','block');
+
+        CLICKED('.brown',()=>{
+         
+            STYLED(CountryDiv,'display','none');
+            
+        })
+
+        REDUX(COUNTRYDATA,(element)=>{
+    
+            CREATEELEMENT('button','LocationButton',(ELEMENT)=>{
+    
+                DISPLAY(ELEMENT,`
+    
+                    <h1 class='LocationText'>${element.name}</h1>
+    
+                    <img class='LocationImage' src='${ICONMODULE}location.png'/>
+                
+                `)
+
+                EVENT(ELEMENT,'click',()=>{
+
+                    Location.value=element.name;
+
+                    DISPLAY(Code,element.phoneCode)
+                 
+                    STYLED(CountryDiv,'display','none');
+                    
+                })
+    
+                ADD(CoutryDivs,ELEMENT);
+            })
+    
+        })
+
+    })
+
+
+
+
+    CLICKED('.forestgreen',()=>{EMAILVERIFICATIONPAGE()});
 
 }
--- app/src/main/assets/Projects/LoginPage/LoginPage.js
+++ app/src/main/assets/Projects/LoginPage/LoginPage.js
@@ -38,35 +38,42 @@ export const LOGINPAGE=()=>{
                     GETPACKAGE(LOGINAPI,'cors',(data)=>{
                         FINDER(data,'Email',Email.value,(user)=>{
                             CONDITION(user.Email === Email.value,
-                                ()=>CHECK(user,(result)=>{
-                                    GETPACKAGE(DELETEACCOUNTGET,'cors',(data)=>{
-                                        FINDER(data,'User',user.SecretCode,(users)=>{
-                                            CONDITION(users.User === user.SecretCode,
-                                                ()=>CHECK(users,(result)=>{
-                                                    MESSAGE('Something Went Wrong');
-                                                    ORIGIN(BUTTON,'Login')
-                                                }),
-                                                ()=>CHECK(users,(result)=>{
-                                                    const DEVICEDATA={
-                                                        "User":user.SecretCode,
-                                                        "Device": getBrowserVersion(),
-                                                       "Date":new Date()
-                                                    }
-                                                    // Functions to get browser and OS information
-                                                    function getBrowserVersion() { return navigator.appVersion; }
-                                                    POSTPACKAGE(DEVICELOGINAPI,'no-cors',DEVICEDATA,(data)=>{
-                                                        STORE('local','User',user.SecretCode);
-                                                        JSONIFICATION(user,(data)=>{
-                                                            STORE('local','UserData',data);
-                                                            HOMEPAGE();
+                                ()=>CONDITION(user.Password === Password.value ,
+                                    ()=>CHECK(user,(result)=>{
+                                        GETPACKAGE(DELETEACCOUNTGET,'cors',(data)=>{
+                                            FINDER(data,'User',user.SecretCode,(users)=>{
+                                                CONDITION(users.User === user.SecretCode,
+                                                    ()=>CHECK(users,(result)=>{
+                                                        MESSAGE('Something Went Wrong');
+                                                        ORIGIN(BUTTON,'Login')
+                                                    }),
+                                                    ()=>CHECK(users,(result)=>{
+                                                        const DEVICEDATA={
+                                                            "User":user.SecretCode,
+                                                            "Device": getBrowserVersion(),
+                                                           "Date":new Date()
+                                                        }
+                                                        // Functions to get browser and OS information
+                                                        function getBrowserVersion() { return navigator.appVersion; }
+                                                        POSTPACKAGE(DEVICELOGINAPI,'no-cors',DEVICEDATA,(data)=>{
+                                                            STORE('local','User',user.SecretCode);
+                                                            JSONIFICATION(user,(data)=>{
+                                                                STORE('local','UserData',data);
+                                                                HOMEPAGE();
+                                                            })
                                                         })
+    
                                                     })
-
-                                                })
-                                            )
+                                                )
+                                            })
                                         })
+                                    }),
+                                    ()=>CHECK(user,(result)=>{
+                                        MESSAGE('Wrong User Password')
+                                        ORIGIN(BUTTON,'Login')
                                     })
-                                }),
+                                
+                                ),
                                 ()=>CHECK(user,(result)=>{
                                     MESSAGE('Wrong User Email')
                                     ORIGIN(BUTTON,'Login')
