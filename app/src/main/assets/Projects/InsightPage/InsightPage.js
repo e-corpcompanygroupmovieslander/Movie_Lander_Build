d56d39975c0bee4fa077b27574bab764538f1ecd
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-21 20:28:44 +0300

    VERSION 4 RELEASE

--- app/src/main/assets/Projects/InsightPage/InsightPage.js
+++ app/src/main/assets/Projects/InsightPage/InsightPage.js
@@ -2,16 +2,48 @@ import { MOVIESDETAILSPAGE } from "../MoviesDetailsPage/MoviesDetailsPage.js"
 
 export const INSIGHTPAGE=()=>{
 
-    BACKHEADERWIDGET(()=>{MOVIESDETAILSPAGE()},
-    `
-        <h1 class='Profile'>Insight</h1>
+    DEJSON('','MovieData',(data)=>{
+        
+        CONDITION(localStorage.getItem(data.MovieName),
+            ()=> DEJSON('local',data.MovieName,(data)=>{
+                STORE('','MovieInsight',data.MyInsight)
+            }),
+            ()=>STORE('','MovieInsight','')
+        
+        )
 
-    `,
-    `
-        <textarea class='InsightInput' placeholder='Enter Your Views On The Movie'></textarea>
+        BACKHEADERWIDGET(()=>{MOVIESDETAILSPAGE()},
+        `
+            <h1 class='Profile'>${data.MovieName}</h1>
+    
+        `,
+        `
+            <textarea class='InsightInput' id='MyView' placeholder='Enter Your Movie Views'></textarea>
+    
+        `,''
+    
+        )
+    
+        DECLARATION('.InsightInput',(ELEMENT)=>{
 
-    `,''
+            ELEMENT.value=sessionStorage.getItem('MovieInsight')
 
-    )
+            EVENT(ELEMENT,'input',()=>{
+
+                const DATA={
+                    "MovieName":data.MovieName,
+                    "MyInsight":ELEMENT.value
+                }
+
+                JSONIFICATION(DATA,(info)=>{
+                    STORE('local',data.MovieName,info)
+                })
+
+            })
+
+        })
+
+    })
+
 
 }
