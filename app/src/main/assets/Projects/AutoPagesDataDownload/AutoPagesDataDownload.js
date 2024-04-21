6f463303181e885bf0987f566721a858002063f8
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-21 07:00:20 +0300

    VERSION 4 RELEASE

--- app/src/main/assets/Projects/AutoPagesDataDownload/AutoPagesDataDownload.js
+++ app/src/main/assets/Projects/AutoPagesDataDownload/AutoPagesDataDownload.js
@@ -1,4 +1,5 @@
-import { CATERGORYAPI, FREEMOVIESAPI } from "../../Modules/Module.js"
+import { CONNECTION } from "../../Connection/Connection.js"
+import { CATERGORYAPI, DELETEACCOUNTGET, FREEMOVIESAPI, LOGINAPI } from "../../Modules/Module.js"
 
 const AUTOAPPDATADOWNLOAD=()=>{
 
@@ -18,6 +19,37 @@ const AUTOAPPDATADOWNLOAD=()=>{
     GETPACKAGE(FREEMOVIESAPI,'cors',(data)=>{
         JSONIFICATION(data,(data)=>{
             STORE('local','FreeMovies',data)
+        })
+    })
+
+    //USER DETAILS UPDATE
+    GETPACKAGE(LOGINAPI,'cors',(data)=>{
+        FINDER(data,'SecretCode',localStorage.getItem('User'),(User)=>{
+            CONDITION(User.SecretCode === localStorage.getItem('User'),
+            ()=>CHECK(User,(result)=>{
+                JSONIFICATION(result,(data)=>{
+                    STORE('local','UserData',data)
+                })
+            }),
+            ()=>CHECK(User,(result)=>{
+                REMOVESTORE('local','UserData');
+            })
+          )
+        })
+    })
+
+    //USER DELETED ACCOUNT
+    GETPACKAGE(DELETEACCOUNTGET,'cors',(data)=>{
+        FINDER(data,'User',localStorage.getItem('User'),(user)=>{
+            CONDITION(user.User === localStorage.getItem('User'),
+            ()=>CHECK(user,(result)=>{
+                CLEARDATA();
+                CONNECTION();
+            }),
+            ()=>CHECK(user,(result)=>{
+                console.log('User Account Still Active')
+            })
+          )
         })
     })
 
