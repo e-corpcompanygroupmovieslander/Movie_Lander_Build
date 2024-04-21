e8c5dead6b35da074cd31314348f1eba19a784b7
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-21 20:30:36 +0300

    VERSION 4 RELEASE

--- app/src/main/assets/Projects/InsightPage/InsightPage.js
+++ app/src/main/assets/Projects/InsightPage/InsightPage.js
@@ -1,47 +1,31 @@
 import { MOVIESDETAILSPAGE } from "../MoviesDetailsPage/MoviesDetailsPage.js"
-
 export const INSIGHTPAGE=()=>{
-
     DEJSON('','MovieData',(data)=>{
-        
         CONDITION(localStorage.getItem(data.MovieName),
             ()=> DEJSON('local',data.MovieName,(data)=>{
                 STORE('','MovieInsight',data.MyInsight)
             }),
             ()=>STORE('','MovieInsight','')
         )
-
         BACKHEADERWIDGET(()=>{MOVIESDETAILSPAGE()},
         `
             <h1 class='Profile'>${data.MovieName}</h1>
-    
         `,
         `
             <textarea class='InsightInput' id='MyView' placeholder='Enter Your Movie Views'></textarea>
-    
         `,''
-    
-        )
-    
+        );
         DECLARATION('.InsightInput',(ELEMENT)=>{
-
-            ELEMENT.value=sessionStorage.getItem('MovieInsight')
-
+            ELEMENT.value=sessionStorage.getItem('MovieInsight');
             EVENT(ELEMENT,'input',()=>{
-
                 const DATA={
                     "MovieName":data.MovieName,
                     "MyInsight":ELEMENT.value
-                }
-
+                };
                 JSONIFICATION(DATA,(info)=>{
-                    STORE('local',data.MovieName,info)
-                })
-
-            })
-
-        })
-
+                    STORE('local',data.MovieName,info);
+                });
+            });
+        });
     })
-
 }
