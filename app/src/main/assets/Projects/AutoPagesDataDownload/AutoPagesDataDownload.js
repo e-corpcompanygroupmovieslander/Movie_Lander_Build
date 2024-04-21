eb3c5dffe67844fc69064ff5f13bf2a522428947
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-21 14:07:22 +0300

    VERSION 4 RELEASE

--- app/src/main/assets/Projects/AutoPagesDataDownload/AutoPagesDataDownload.js
+++ app/src/main/assets/Projects/AutoPagesDataDownload/AutoPagesDataDownload.js
@@ -1,5 +1,5 @@
 import { CONNECTION } from "../../Connection/Connection.js"
-import { CATERGORYAPI, DELETEACCOUNTGET, FREEMOVIESAPI, LOGINAPI } from "../../Modules/Module.js"
+import { CATERGORYAPI, DELETEACCOUNTGET, FREEMOVIESAPI, LOGINAPI, MTNPREMIUMPAYGET } from "../../Modules/Module.js"
 
 const AUTOAPPDATADOWNLOAD=()=>{
 
@@ -52,6 +52,24 @@ const AUTOAPPDATADOWNLOAD=()=>{
           )
         })
     })
+
+    //AUTOPREMIUM USERS
+    GETPACKAGE(MTNPREMIUMPAYGET, 'cors', (data) => {
+        REVERSE(data,(result)=>{
+            FINDER(result, 'User', localStorage.getItem('User'), (user) => {
+                if (new Date() <= new Date(user.ExpiryDate)) {
+                    CHECK(user, (result) => {
+                        STORE('local', 'PremiumUser', JSON.stringify(result));
+                        STORE('local', 'Premium', 'ON');
+                    });
+                } else {
+                    REMOVESTORE('local', 'PremiumUser');
+                    REMOVESTORE('local', 'Premium');
+                }
+            });
+        });    
+    });
+    
 
 }
 export{AUTOAPPDATADOWNLOAD}
--- app/src/main/assets/Projects/SyncAppData/AutoSyncData.js
+++ app/src/main/assets/Projects/SyncAppData/AutoSyncData.js
@@ -80,6 +80,24 @@ export const AUTOSYNC=()=>{
             })
         })
 
+        //AUTOPREMIUM USERS
+        GETPACKAGE(MTNPREMIUMPAYGET, 'cors', (data) => {
+            REVERSE(data,(result)=>{
+                FINDER(result, 'User', localStorage.getItem('User'), (user) => {
+                    if (new Date() <= new Date(user.ExpiryDate)) {
+                        CHECK(user, (result) => {
+                            STORE('local', 'PremiumUser', JSON.stringify(result));
+                            STORE('local', 'Premium', 'ON');
+                        });
+                    } else {
+                        REMOVESTORE('local', 'PremiumUser');
+                        REMOVESTORE('local', 'Premium');
+                    }
+                });
+            });    
+        });
+    
+
     })
 
 }
