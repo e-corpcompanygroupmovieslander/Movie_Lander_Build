be95a053f36344f4e4f8980f401c143bf1f4606b
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-23 12:03:22 +0300

    VERSION 4 RELEASE

---
+++ app/src/main/assets/Projects/HomePage/NonParentalControl.js
@@ -0,0 +1,82 @@
+import { ICONMODULE } from "../../Modules/Module.js"
+
+export const NONPARENTALCONTROLSECTION=`
+<div id='LatestAnimation' class='View'>
+    <img id='AnimationsLoader' class='LoadingIcon' src='${ICONMODULE}loading.png'/>                
+</div>
+
+<div id='homeHolder' class='View'>
+    <header class='AppHeader'>
+        <h1 class='SectionName' >Animations</h1>
+        <img id='MoreAnimations' class='SeeMoreIcon' src='${ICONMODULE}arrow.png'/>            
+    </header>
+    <div id='Animations' class='View'>
+        <img id='AnimationsLoader' class='LoadingIcon' src='${ICONMODULE}loading.png'/>        
+    </div>            
+</div>
+
+<div id='homeHolder' class='View'>
+    <header class='AppHeader'>
+        <h1 class='SectionName' >Action</h1>
+        <img id='MoreMovies' class='SeeMoreIcon' src='${ICONMODULE}arrow.png'/>
+    </header>
+    <div id='Movies' class='View'>
+        <img id='AnimationsLoader' class='LoadingIcon' src='${ICONMODULE}loading.png'/>           
+    </div>            
+</div>
+
+<div id='LatestMovies' class='View'>
+    <img id='AnimationsLoader' class='LoadingIcon' src='${ICONMODULE}loading.png'/>            
+</div>
+
+<div id='homeHolder' class='View'>
+    <header class='AppHeader'>
+        <h1 class='SectionName' >Marathons</h1>
+        <img id='MoreMarathons' class='SeeMoreIcon' src='${ICONMODULE}arrow.png'/>                
+    </header>
+    <div id='Marathons' class='View'>
+        <img id='AnimationsLoader' class='LoadingIcon' src='${ICONMODULE}loading.png'/>            
+    </div>            
+</div>
+
+<div id='homeHolder' class='View'>
+    <header class='AppHeader'>
+        <h1 class='SectionName' >Series</h1>
+        <img id='MoreSeries' class='SeeMoreIcon' src='${ICONMODULE}arrow.png'/>               
+    </header>
+    <div id='Series' class='View'>
+        <img id='AnimationsLoader' class='LoadingIcon' src='${ICONMODULE}loading.png'/>                
+    </div>
+</div>
+
+<div id='LatestNigerian' class='View'>
+    <img id='AnimationsLoader' class='LoadingIcon' src='${ICONMODULE}loading.png'/>            
+</div>
+
+<div id='homeHolder' class='View'>
+    <header class='AppHeader'>
+        <h1 class='SectionName' >Adventure</h1>
+        <img id='MoreAdventure' class='SeeMoreIcon' src='${ICONMODULE}arrow.png'/>
+    </header>
+    <div id='Adventure' class='View'>
+        <img id='AnimationsLoader' class='LoadingIcon' src='${ICONMODULE}loading.png'/>            
+    </div>
+</div>
+
+<div id='homeHolder' class='View'>
+    <header class='AppHeader'>
+        <h1 class='SectionName' >Horror</h1>
+        <img id='MoreHorror' class='SeeMoreIcon' src='${ICONMODULE}arrow.png'/>                
+    </header>
+    <div id='Horror' class='View'>
+        <img id='AnimationsLoader' class='LoadingIcon' src='${ICONMODULE}loading.png'/>            
+    </div>
+</div>
+
+<div id='LatestRomance' class='View'>
+    <img id='AnimationsLoader' class='LoadingIcon' src='${ICONMODULE}loading.png'/>             
+</div>
+
+<br><br><br>
+
+`
---
+++ app/src/main/assets/Projects/HomePage/ParentalControlSection.js
@@ -0,0 +1,20 @@
+import { ICONMODULE } from "../../Modules/Module.js"
+
+export const PARENTALCONTROLSECTION=`
+<div id='LatestAnimation' class='View'>
+    <img id='AnimationsLoader' class='LoadingIcon' src='${ICONMODULE}loading.png'/>                
+</div>
+
+<div id='homeHolder' class='View'>
+    <header class='AppHeader'>
+        <h1 class='SectionName' >Animations</h1>
+        <img id='MoreAnimations' class='SeeMoreIcon' src='${ICONMODULE}arrow.png'/>            
+    </header>
+    <div id='Animations' class='View'>
+        <img id='AnimationsLoader' class='LoadingIcon' src='${ICONMODULE}loading.png'/>        
+    </div>            
+</div>
+
+<br><br><br>
+
+`
