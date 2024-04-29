482e89bd565053b4dc6cec54d192add25bf71895
E-corp Company Group <e.corpcompanygroup27@gmail.com>
2024-04-29 13:04:05 +0300

    VERSION 5 RELEASE

--- app/src/main/assets/Projects/AppVersionChecker/AppVersionChecker.js
+++ app/src/main/assets/Projects/AppVersionChecker/AppVersionChecker.js
@@ -1,3 +1,4 @@
+import { CONNECTION } from "../../Connection/Connection.js";
 import { ICONMODULE } from "../../Modules/Module.js"
 
 export const APPVERSIONCHECKER=()=>{
@@ -6,10 +7,34 @@ export const APPVERSIONCHECKER=()=>{
 
         const CURRENTVERSION='0.0.5';
 
-        CONDITION( CURRENTVERSION === localStorage.getItem('AppVersion') ,
-        
-            ()=>console.log('SameVersion'),
-            ()=>CHECK(localStorage.getItem('AppVersion'),(result)=>{
+        CONDITION(localStorage.getItem('AppVersion'),
+        ()=>CONDITION(localStorage.getItem('AppVersion') === CURRENTVERSION ,
+            ()=>console.log('Same Version'),
+            ()=>CHECK(CURRENTVERSION,(result)=>{
+
+                WIDGET(`
+
+                    <img class='AppLogo' src='${IMAGESPATH}app_icon.png'/>
+                    <h1 class='LoaderMessage'>Update Required</h1>
+                    <p>
+                    Movie Lander Version ${localStorage.getItem('AppVersion')}
+                    <br><br>
+                    To Access Some New Features Please Update to the Latest Version Of Movie Lander
+                    </p>
+                    <img id='OfflineImage' src='${ICONMODULE}upload.png'/>
+                    <button class='forestgreen'>Download</button>
+
+                `)
+
+                const UPDATELINK='https://play.google.com/store/apps/details?id=com.movie_lander&pcampaignid=web_share';
+
+                CLICKED('.forestgreen',()=>{WEBSITE(UPDATELINK)})
+
+                }
+            )
+    
+        ),
+        ()=>CHECK(CURRENTVERSION,(result)=>{
 
                 WIDGET(`
 
